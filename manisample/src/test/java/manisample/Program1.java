9aa8a14dc8ff23a4f9e07084834f197bc707415a
Admin <Admin@DESKTOP-SHFQC7C>
2022-07-20 22:54:52 +0530

    First upload

---
+++ manisample/src/test/java/manisample/Program1.java
@@ -0,0 +1,18 @@
+package manisample;
+
+import org.testng.annotations.Test;
+
+public class Program1 {
+	@Test
+	public void demo() {
+		System.out.println("First jenkins project");
+		System.out.println("Second maven project");
+	}
+
+	@Test
+	public void democycle() {
+		System.out.println("First jenkins project");
+		System.out.println("Second maven project");
+	}
+
+}
